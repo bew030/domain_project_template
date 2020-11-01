c8e1f60e328778a18dad75f6ce6e776ffc1036e0
Bernard Wong <bernardwong@Bernards-iMac.local>
2020-10-31 23:33:04 -0700

    added packages

--- src/data/data.py
+++
@@ -1,5 +0,0 @@
-# simulates data based on information pulled from research or current
-# situations 
-
-def make_data(infection_rate, social_distancing_factor, region):
-    
--- src/model/model.py
+++
@@ -1,4 +0,0 @@
-# simulates the model step by step 
-
-def agent_based_model(steps, agents, environment):
-    
